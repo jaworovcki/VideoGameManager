894f1d39a5a5fd0874dd0f200018a5db6c597081
Andrii <jaworovcki@gmail.com>
2023-08-27 18:14:22 +0300

    Modify appcomponent

--- VideoGameManagerUI/src/app/app.component.ts
+++ VideoGameManagerUI/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+export interface GameGenre {
+  id: number;
+  name: string;
+}
+
+export interface Game {
+  id: number;
+  name: string;
+  genre: GameGenre | null;
+  personalRating: number;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'VideoGameManagerUI';
+
+export class AppComponent implements OnInit {
+  public games: Game[] = [];
+
+  constructor(private client: HttpClient) { }
+
+  ngOnInit() {
+    this.client.get<Game[]>('https://localhost:7135/api/games')
+    .subscribe(result => this.games = result);
+    }
 }
