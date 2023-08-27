bf7dc3dca1fdefea4d368e564e55a91caf33c495
Andrii <jaworovcki@gmail.com>
2023-08-27 14:35:52 +0300

    Create a database model

---
+++ DataAccess/Model.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VideoGameManager.DataAccess
+{
+    public class GameGenre
+    {
+        public int Id { get; set; }
+
+        [MaxLength(250)]
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        public List<Game> Games { get; set; } = new();
+    }
+
+    public class Game
+    {
+        public int Id { get; set; }
+
+        [MaxLength(250)]
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        public GameGenre Genre { get; set; } = null!;
+
+        public int PersonalRating { get; set; }
+    }
+}
